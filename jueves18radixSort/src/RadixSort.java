0f9e72997dc246b94e2b3ac87ac7c49343fc5436
Fabrizio <fabrizio>
2021-03-18 23:23:46 -0300

    string utils done

--- jueves18radixSort/src/RadixSort.java
+++ jueves18radixSort/src/RadixSort.java
@@ -1,17 +1,70 @@
+import java.util.Arrays;
+
 public class RadixSort {
-    public static void radixSort(int []arr)
-    {
+    public static void radixSort(int[] arr) {
+
         // PROGRAMAR AQUI
+
     }
 
-    public static void main(String[] args)
-    {
-        int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
+    public static void main(String[] args) {
+
+        // "Tests" ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        System.out.println("Here are ten F:  "+StringUtils.replicate('F', 10));
+
+
+        System.out.println("0090 = "+StringUtils.lpad("90", 4, '0'));
+
+
+        int[] intArray = new int[4];
+        intArray[0]=1;
+        intArray[1]=2;
+        intArray[2]=3;
+        intArray[3]=4;
+
+        System.out.println("[1 ,2 ,3 ,4] = "+ Arrays.toString(StringUtils.toStringArray(intArray)));
+
+
+        String[] stringArray = new String[4];
+        stringArray[0]="1";
+        stringArray[1]="2";
+        stringArray[2]="3";
+        stringArray[3]="4";
+
+        System.out.println("[1 ,2 ,3 ,4] = "+ Arrays.toString(StringUtils.toIntArray(stringArray)));
+
+
+        String[] stringArray1 = new String[4];
+        stringArray1[0]="A";
+        stringArray1[1]="BBBBB";
+        stringArray1[2]="CC";
+        stringArray1[3]="DD";
+
+        System.out.println("5 = " + StringUtils.maxLength(stringArray1));
+
+
+        String[] stringArray2 = new String[4];
+        stringArray2[0]="1";
+        stringArray2[1]="55555";
+        stringArray2[2]="22";
+        stringArray2[3]="333";
+
+        StringUtils.lNormalize(stringArray2, '0');
+
+        System.out.println("[00001, 55555, 00022, 00333] = " + Arrays.toString(stringArray2));
+
+
+        
+        // Tests end ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+        int arr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8};
+
         radixSort(arr);
 
-        for(int i=0; i<arr.length;i++)
-        {
-            System.out.print(arr[i]+(i<arr.length-1?",":""));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + (i < arr.length - 1 ? "," : ""));
         }
     }
 }
--- jueves18radixSort/src/StringUtils.java
+++ jueves18radixSort/src/StringUtils.java
@@ -1,44 +1,77 @@
 public class StringUtils {
+
     // Retorna una cadena compuesta por n caracteres c
     // Ejemplo: replicate('x',5) ==> 'xxxxx'
-    public static String replicate(char c, int n)
-    {
-        return "";
+    public static String replicate(char c, int n) {
+        String string = Character.toString(c);
+        return string.repeat(n);
     }
 
-    // Retorna una cadena de longitud n, compuesta por s
-    // y precedida de tantos caracteres c como sea necesario
+    // Retorna una cadena de longitud length, compuesta por string
+    // y precedida de tantos caracteres filler como sea necesario
     // para completar la longitud mencionada
     // Ejemplo lpad("5",3,'0') ==> "005"
-    public static String lpad(String s, int n, char c)
-    {
-        return "";
+    public static String lpad(String string, int length, char filler) {
+        int fillCount = length - string.length();
+
+        String filling = replicate(filler, fillCount);
+
+        return filling + string;
     }
 
-    // Retorna un String[] conteniendo los elementos de arr
+    // Retorna un String[] conteniendo los elementos de array
     // representados como cadenas de caracteres
-    public static String[] toStringArray(int arr[])
-    {
-        return null;
+    public static String[] toStringArray(int[] arr) {
+        int length = arr.length;
+
+        Integer[] integerArray = new Integer[length];
+
+        String[] stringArray = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            integerArray[i] = arr[i];
+            stringArray[i] = integerArray[i].toString();
+        }
+
+        return stringArray;
     }
 
-    // Retorna un String[] conteniendo los elementos de arr
-    // representados como cadenas de caracteres
-    public static int[] toIntArray(String arr[])
-    {
-        return null;
+    // Retorna un int[] conteniendo los elementos de arr
+    public static int[] toIntArray(String[] arr) {
+        int length = arr.length;
+
+        int[] intArray = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            intArray[i] = Integer.parseInt(arr[i]);
+        }
+
+        return intArray;
     }
 
     // Retorna la longitud del elemento con mayor cantidad
     // de caracteres del array arr
-    public static int maxLength(String arr[])
-    {
-        return 0;
+    public static int maxLength(String[] arr) {
+        int max = 0;
+
+        for (String s : arr) {
+            int elementSize = s.length();
+            if (elementSize > max) {
+                max = elementSize;
+            }
+        }
+        return max;
     }
 
-    // Completa los elemento del arr agregando caracteres c
-    // a la izquierda, dejando a todos con la longitud del mayor
-    public static void lNormalize(String arr[], char c)
-    {
+    // Completa los elementos del arr agregando caracteres c
+    // a la izquierda, dejando a todos con la longitud del mayor elemento de arr
+    public static void lNormalize(String[] arr, char c) {
+        int maxLength = maxLength(arr);
+
+        int length = arr.length;
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = lpad(arr[i], maxLength, c);
+        }
     }
 }
