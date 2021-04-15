e2cbcb7d6259dcca181c52a6fd1e548ac7255b70
Fabrizio <fabrizio>
2021-04-15 15:39:50 -0300

    starting quality challenge

---
+++ DesafioQuality/src/main/java/com/example/desafioquality/controllers/FlightController.java
@@ -0,0 +1,12 @@
+package com.example.desafioquality.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlightController {
+
+    // get /flights
+
+    // post /flight-reservation
+
+}
---
+++ DesafioQuality/src/main/java/com/example/desafioquality/controllers/HotelController.java
@@ -0,0 +1,12 @@
+package com.example.desafioquality.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelController {
+
+    // get /hotels
+
+    // post /booking
+}
